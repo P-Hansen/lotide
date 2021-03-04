3d96e33fe1107ef9d45ae1e5dfe980cc35c231fc
P-hansen <philip.pt.hansen@gmail.com>
2021-03-04 03:15:45 +0000

    letterPositions.js

---
+++ letterPositions.js
@@ -0,0 +1,14 @@
+const letterPositions = function (string) {
+  let tally = {};
+  for (let i = 0; i < string.length; i++) {
+    if ((tally[string[i]] === undefined) &&  (string[i] !== ' ')){
+      tally[string[i]] = [i];
+    }
+    else if (string[i] !== ' ') {
+      tally[string[i]].push(i);
+    }
+  }
+  return tally
+}
+
+console.log(letterPositions("lighthouse in the house"));
