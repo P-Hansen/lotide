5636a0d5a9ed30c139a9861cd1bbc3e41616da46
P-hansen <philip.pt.hansen@gmail.com>
2021-03-04 03:24:15 +0000

    findKeyByValue.js

---
+++ findKeyByValue.js
@@ -0,0 +1,27 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`\u2705 \u2705 \u2705 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`\u26D4 \u26D4 \u26D4 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function (Object, value) {
+  for (const key in Object) {
+    if (Object[key] === value) {
+      return key
+    }
+  }
+}
+
+// TEST CODE
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire",
+  animated: "The Simpsons"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
