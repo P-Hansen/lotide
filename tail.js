90f1cd02534a651b42ea18ed6e3f66a60845db18
P-hansen <philip.pt.hansen@gmail.com>
2021-03-08 23:15:56 +0000

    tail/tailTest moch

--- tail.js
+++ tail.js
@@ -1,19 +1,11 @@
 const assertEqual = require('./assertEqual');
 
 const tail = function(array) {
-  if ((array.length === 0) || (array.length === 1)) {
+  if (array.length === 0) {
     return [];
   } else {
-    return array.slice(1, array.length);
+    return array[array.length - 1];
   }
 };
 
 module.exports = tail;
-
-// TEST CODE
-/*const words = ["Yo Yo", "Lighthouse", "Labs", "More", "words", "here"];
-for (let i = 1; i < words.length; i++) {
-  console.log(words[i]);
-}
-console.log(tail(words)); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 6); // original array should still have 3 elements!*/
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,10 +1,29 @@
-const tail = require("../tail");
-const assertEqual = require("../assertEqual");
+const assert = require('chai').assert;
+const tail = require('../tail');
 
-const words = ["Yo Yo", "Lighthouse", "Labs", "More", "words", "here"];
+describe("#tail", () => {
+  it("returns 4 for [1, 2, 3, 4]", () => {
+    assert.strictEqual(tail([1, 2, 3, 4]), 4);
+  });
 
-for (let i = 1; i < words.length; i++) {
-  console.log(words[i]);
-}
-console.log(tail(words)); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 6); // original array should still have 3 elements!
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(tail(['5']), '5');
+  });
+
+  it("returns [] for []", () => {
+    assert.strictEqual(tail([]).length, [].length);
+  });
+
+  it("returns 'here' for ['Yo Yo', 'Lighthouse', 'Labs', 'More', 'words', 'here']", () => {
+    const words = ["Yo Yo", "Lighthouse", "Labs", "More", "words", "here"];
+    assert.strictEqual(tail(words), "here");
+  });
+
+  it("keeps the original array intact, ['Yo Yo', 'Lighthouse', 'Labs', 'More', 'words', 'here'].length remains unchanged", () => {
+    const words = ["Yo Yo", "Lighthouse", "Labs", "More", "words", "here"];
+    const lengthOfWordsBefore = words.length;
+    tail(words);
+    assert.strictEqual(words.length, lengthOfWordsBefore);
+  });
+
+});
